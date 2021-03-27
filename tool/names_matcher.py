757fdc5c7dcb3d30c42363937d411d62922d8177
WerLaj <weronikalajewska@gmail.com>
2021-03-27 12:07:43 +0100

    code refactor - part IV

--- tool/names_matcher.py
+++ tool/names_matcher.py
@@ -3,10 +3,9 @@ from fuzzywuzzy import fuzz
 import spacy
 from spacy import displacy, gold
 from spacy.tokens import Span
-import os
 import itertools
 
-from tool.file_and_directory_management import read_file_to_list, write_list_to_file, read_file, write_text_to_file, read_sentences_from_file
+from tool.file_and_directory_management import write_list_to_file
 from tool.gender_checker import get_name_gender, get_personal_titles, create_titles_and_gender_dictionary
 from tool.diminutives_recognizer import get_names_from_diminutive
 
